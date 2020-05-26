97b76bdbc2110d11ee44408610e0ec73af01b4bb
Nicholas <vankalif@gmail.com>
2020-05-26 17:00:29 +0300

    Правки

--- UI/main_window.py
+++ UI/main_window.py
@@ -10,8 +10,20 @@ class MainWindow(tk.Frame):
     def init_main(self):
         tab_control = ttk.Notebook()
         ess_tab = ttk.Frame(tab_control)
+        kis_tab = ttk.Frame(tab_control)
+        pyat_tab = ttk.Frame(tab_control)
+        jel_tab = ttk.Frame(tab_control)
+        archive_tab = ttk.Frame(tab_control)
         tab_control.add(ess_tab, text="ЕЭУ")
+        tab_control.add(kis_tab, text="КЭУ")
+        tab_control.add(pyat_tab, text="ПЭУ")
+        tab_control.add(jel_tab, text="ЖЭУ")
+        tab_control.add(archive_tab, text="Архивы")
         tab_control.pack(expand=1, fill="both")
+
+
+class SensorFrame(tk.LabelFrame):
+    pass
 
 
 if __name__ == '__main__':
