c37a4f27f080109b18077b4892754a48bb499eb0
Nicholas <vankalif@gmail.com>
2020-05-26 15:39:28 +0300

    Правки

--- UI/main_window.py
+++ UI/main_window.py
@@ -1,26 +1,23 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 
-class MainWindowTabs:
+class MainWindow(tk.Frame):
     def __init__(self, root):
-        # навигатор вкладок
-        tab_control = ttk.Notebook(main_window)
-        # ессентукская вкладка
+        super().__init__(root)
+        self.init_main()
+
+    def init_main(self):
+        tab_control = ttk.Notebook()
         ess_tab = ttk.Frame(tab_control)
-        # добавить вкладку на окно
         tab_control.add(ess_tab, text="ЕЭУ")
-        # расположить на главном окне
         tab_control.pack(expand=1, fill="both")
 
-        skv_frame = LabelFrame(ess_tab, text="5/0", width="50", height="50", bg="gray")
-        skv_frame.grid(row=0, column=0)
 
-
-# конфигурвция главного окна
-main_window = Tk()
-main_window.title("Мониторинг")
-main_window.geometry("1400x600")
-main_window.resizable(0, 0)
-tabs = MainWindowTabs(main_window)
-main_window.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = MainWindow(root)
+    root.title("Мониторинг")
+    root.geometry("1400x600")
+    root.resizable(False, False)
+    root.mainloop()
