4c9e9a8ab967472a51ea64229dc4ecb205d0aaaf
Nicholas <vankalif@gmail.com>
2020-06-05 10:41:45 +0300

    Небольшие изменения

--- ui/remote_site_tab.py
+++ ui/remote_site_tab.py
@@ -7,20 +7,14 @@ class RemoteSiteTab(ttk.Frame):
         super().__init__(root, **kw)
         self.boreholes = boreholes
         self.site = site
-        self.frames = []
+        self.frames = [SensorFrame(self, text=borehole_name) for borehole_name in self.boreholes[site]]
         self.tab_columns = 0
         self.tab_row = 0
-        for borehole_name in self.boreholes[site]:
+        for sensor_frame in self.frames:
             if self.tab_columns != 5:
-                self.frames.append(SensorFrame(self, text=borehole_name).grid(row=self.tab_row,
-                                                                              column=self.tab_columns,
-                                                                              padx=10,
-                                                                              pady=5))
+                sensor_frame.grid(row=self.tab_row, column=self.tab_columns, padx=10, pady=5)
                 self.tab_columns += 1
             else:
                 self.tab_columns = 0
                 self.tab_row += 1
-                self.frames.append(SensorFrame(self, text=borehole_name).grid(row=self.tab_row,
-                                                                              column=self.tab_columns,
-                                                                              padx=10,
-                                                                              pady=5))
+                sensor_frame.grid(row=self.tab_row, column=self.tab_columns, padx=10, pady=5)
